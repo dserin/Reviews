0dd658ce4eedce1c538e0d39ce3f2e4c40bd5e92
dserin <dogacanserin@gmail.com>
2022-08-13 13:34:06 +0300

    .

---
+++ src/data.js
@@ -0,0 +1,41 @@
+const reviews = [
+    {
+      id: 1,
+      name: 'Özen Taştan',
+      job: 'web developer',
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+      text:
+        "Web developers create and maintain websites. They are also responsible for the site's technical aspects, such as its performance and capacity, which are measures of a website's speed and how much traffic the site can handle. In addition, web developers may create content for the site.",
+    },
+    {
+      id: 2,
+      name: 'MeteHan Bacı',
+      job: 'Front-End Developer',
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
+      text:
+        'A front-end developer creates websites and applications using web languages such as HTML, CSS, and JavaScript that allow users to access and interact with the site or app. When you visit a website, the design elements you see were created by a front-end developer.',
+    },
+    {
+      id: 3,
+      name: 'Kutay Topaç',
+      job: 'intern',
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+      text:
+        'It is the applied learning period of a person who will acquire a profession and the period that this person spends working in one or more departments of an institution in order to increase his professional knowledge.',
+    },
+    {
+      id: 4,
+      name: 'Furkan Yazıcı',
+      job: 'Data Analyst',
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+      text:
+        'Data analysis is a process of inspecting, cleansing, transforming, and modelling data with the goal of discovering useful information, informing conclusions, and supporting decision-making. Data analysis has multiple facets and approaches, encompassing diverse techniques under a variety of names, and is used in different business, science, and social science domains. In todays business world, data analysis plays a role in making decisions more scientific and helping businesses operate more effectively. ',
+    },
+  ];
+  
+  export default reviews;
+  
